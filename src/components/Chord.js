1f81604c812ae87cb813fc06bdc2d706a42d707c
Davit Petrosyan <64999212+DavitPetro@users.noreply.github.com>
2022-08-02 19:10:27 +0000

    Chord Buttons, Backend

--- src/components/Chord.js
+++ src/components/Chord.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
-export default function Task( {chord, switchChord} ) {
+export default function Task( {chord, switchChord, soundChord} ) {
   
   function handleNextClick() {
     switchChord(chord.name)
   }
+
+  function handleSoundClick() {
+    soundChord(chord.name)
+  }
   
   return (
-    <div id = {chord.name}>
-
-
+    <div id = {chord.name} style={{visibility: hidden}}>
+        <button onClick={handleSoundClick}>Sound Chord</button>
+        <button onClick={handleNextClick}>Next <i class="fa-solid fa-arrow-right"></i></button>
     </div>
   )
 }
--- src/components/OpenChords.js
+++ src/components/OpenChords.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Chords from './Chord'
 
-function OpenChords ( {chords, switchChord} ) {
+function OpenChords ( {chords, switchChord, soundChord} ) {
     return (
         chords.map(chord => {
             return <Chord key = {chord.name} switchChord = {switchChord} chord = {chord}/>
